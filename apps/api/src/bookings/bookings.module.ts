a02bd2d84d452d1102f5ff37b49e3e0e635085ad
Marcos Stenves <marcosstevens2012@gmail.com>
2025-09-17 15:06:23 -0300

    fix: add jwtmodule import to bookings module to resolve dependency injection error

--- apps/api/src/bookings/bookings.module.ts
+++ apps/api/src/bookings/bookings.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { DatabaseModule } from "../database/database.module";
 import { PaymentsModule } from "../payments/payments.module";
 import { BookingsController } from "./bookings.controller";
 import { BookingsService } from "./bookings.service";
 
 @Module({
-  imports: [DatabaseModule, PaymentsModule],
+  imports: [DatabaseModule, PaymentsModule, JwtModule],
   controllers: [BookingsController],
   providers: [BookingsService],
   exports: [BookingsService],
